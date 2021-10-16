7d3de6097047736683a08c8458917dff3ea75c98
agoranomus <51297305+agoranomus@users.noreply.github.com>
2021-10-16 22:56:37 +0300

    Update allspot.sol

--- allspot.sol
+++ allspot.sol
@@ -1,5 +1,7 @@
 // contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
+// Trademark (R) 2021 Allspot LTD
+// Allspot LTD is a PRIVATE LIMITED COMPANY registred in the UK. Company Registraition number 13288177
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
