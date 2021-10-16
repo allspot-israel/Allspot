31bb21702d3a31bb3f70bdbc3549ee0ab115cdb5
agoranomus <51297305+agoranomus@users.noreply.github.com>
2021-10-16 22:53:27 +0300

    Update allspot_flatten.sol

--- allspot_flatten.sol
+++ allspot_flatten.sol
@@ -1,7 +1,7 @@
 
 // File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol
-// MIT License 
-//Allspot LTD
+// MIT License
+// Copyright (c) 2021 Allspot LTD
 
 
 pragma solidity ^0.8.0;
